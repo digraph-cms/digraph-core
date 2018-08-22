e0f3c44e15385350dec40f387919ff3cfe1cec4b
Joby Elliott <joby@Jobys-iMac.local>
2018-08-22 13:54:48 -0600

    digraph-specific virtual columns

--- src/DSO/ContentFactory.php
+++ src/DSO/ContentFactory.php
@@ -9,6 +9,35 @@ class ContentFactory extends Factory
 {
     protected $cms;
 
+    protected $virtualColumns = [
+        'dso.id' => [
+            'name'=>'dso_id',
+            'type'=>'VARCHAR(16)',
+            'index' => 'BTREE',
+            'unique' => true
+        ],
+        'dso.type' => [
+            'name'=>'dso_type',
+            'type'=>'VARCHAR(30)',
+            'index'=>'BTREE'
+        ],
+        'dso.deleted' => [
+            'name'=>'dso_deleted',
+            'type'=>'BIGINT',
+            'index'=>'BTREE'
+        ],
+        'digraph.slug' => [
+            'name'=>'digraph_slug',
+            'type'=>'VARCHAR(100)',
+            'index'=>'BTREE'
+        ],
+        'digraph.parent.0' => [
+            'name'=>'digraph_parent_0',
+            'type'=>'VARCHAR(100)',
+            'index'=>'BTREE'
+        ]
+    ];
+
     public function class(array $data) : ?string
     {
         return Noun::class;
