6ee0958aa9e98b936f9d353a6aa2386f9723be30
Joby Elliott <joby@Jobys-iMac.local>
2018-09-18 10:33:14 -0600

    made whether content-length is sent configurable

--- src/Mungers/Render/Output.php
+++ src/Mungers/Render/Output.php
@@ -13,9 +13,13 @@ class Output extends AbstractMunger
         to have that file passed directly through via readfile()
          */
         if ($package['response.readfile']) {
-            //send a content length header so that download progress works
-            $size = filesize($package['response.readfile']);
-            @header('Content-Length:'.$size);
+            //allow config to skip sending content length, because it doesn't
+            //work right on all servers
+            if ($this->cms->config['send-content-length-header']) {
+                //send a content length header so that download progress works
+                $size = filesize($package['response.readfile']);
+                @header('Content-Length:'.$size);
+            }
             //send file to browser
             readfile($package['response.readfile']);
             return;
