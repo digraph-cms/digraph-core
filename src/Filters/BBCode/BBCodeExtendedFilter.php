71b134085ce64d4eb6c2d71f3ade051705d2a4e7
Joby Elliott <elhober@unm.edu>
2019-01-03 12:51:59 -0700

    the right class name

--- src/Filters/BBCode/BBCodeExtendedFilter.php
+++ src/Filters/BBCode/BBCodeExtendedFilter.php
@@ -2,7 +2,7 @@
 /* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
 namespace Digraph\Filters\BBCode;
 
-class BBCodeAdvancedFilter extends AbstractBBCodeFilter
+class BBCodeExtendedFilter extends AbstractBBCodeFilter
 {
     const TEMPLATEPREFIX = '_bbcode/extended/';
 }
