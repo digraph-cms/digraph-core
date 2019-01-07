c3b4dd124e248da4109e592d998f559d810d3066
Joby Elliott <elhober@unm.edu>
2019-01-07 11:33:33 -0700

    fixes for tables

--- src/Filters/XSSFilter.php
+++ src/Filters/XSSFilter.php
@@ -7,6 +7,9 @@ class XSSFilter extends AbstractFilter
     public function filter(string $text, array $opts = []) : string
     {
         $filter = new \lincanbin\WhiteHTMLFilter();
+        $filter->config->modifyTagWhiteList([
+            'thead' => []
+        ]);
         $filter->loadHTML($text);
         $filter->clean();
         return $filter->outputHTML();
