4fb42f8baff1b98a60cd29f56f04ae33389385cd
Joby Elliott <code@byjoby.com>
2019-01-13 10:26:01 -0700

    vastly improved PDF system
    This probably won't be the final form of it. I may rethink the core 
    rendering munger just a bit, and see if it can become more extensible. 
    That way any verb could output content for mpdf and mark it to be 
    rendered as a PDF rather than an HTML page.

--- modules/digraph_pdf/src/PdfHelper.php
+++ modules/digraph_pdf/src/PdfHelper.php
@@ -6,31 +6,60 @@ use Flatrr\SelfReferencingFlatArray;
 
 class PdfHelper extends \Digraph\Helpers\AbstractHelper
 {
+    protected function config($noun=null)
+    {
+        $config = new SelfReferencingFlatArray($this->cms->config->get('pdf'));
+        if ($noun && $noun['pdf']) {
+            $config->merge($noun['pdf'], null, true);
+        }
+        return $config;
+    }
+
     public function pdfBook($noun)
     {
         $mpdf = $this->mpdf($noun);
-        $mpdf->TOCpagebreakByArray($this->cms->config['pdf.toc']);
-        $this->buildPDFBook($mpdf, $noun);
         $this->mpdfNoun($mpdf, $noun);
+        $tc = $this->config($noun)['toc'];
+        $mpdf->TOCpagebreakByArray($tc);
+        $this->buildPDFBook($mpdf, $noun);
+        $mpdf->setHTMLHeader('', 'O');
+        $mpdf->setHTMLHeader('', 'E');
         return $mpdf->output('', 'S');
+    }
+
+    protected function bookMeta($noun)
+    {
+        return $this->getTemplate('book_meta', $noun);
     }
 
     protected function buildPDFBook(&$mpdf, $noun, $level=0)
     {
+        $this->mpdfNoun($mpdf, $noun);
         //write this noun's content to the PDF if it isn't a TOC
         if (!$this->isTOC($noun)) {
-            $this->mpdfNoun($mpdf, $noun);
-            //add page break
+            //different behavior for first/root noun
             if ($level == 0) {
-                $mpdf->AddPageByArray([
-                    'type' => 'odd'
-                ]);
+                //root noun doesn't get a page break, but does need headers and
+                //footer set to its right ones
+                $mpdf->SetHeaderByName($noun['dso.id'].'_right', 'O');
+                $mpdf->SetHeaderByName($noun['dso.id'].'_left', 'E');
+                $mpdf->SetFooterByName($noun['dso.id'].'_right', 'O');
+                $mpdf->SetFooterByName($noun['dso.id'].'_left', 'E');
             } else {
-                $mpdf->AddPageByArray([
-                    'type' => 'next-odd'
+                $mpdf->addPageByArray([
+                    'type' => 'next-odd',
+                    'odd-header-name' => 'html_'.$noun['dso.id'].'_right',
+                    'odd-header-value' => 1,
+                    'even-header-name' => 'html_'.$noun['dso.id'].'_left',
+                    'even-header-value' => 1,
+                    'odd-footer-name' => 'html_'.$noun['dso.id'].'_right',
+                    'odd-footer-value' => 1,
+                    'even-footer-name' => 'html_'.$noun['dso.id'].'_left',
+                    'even-footer-value' => 1
                 ]);
             }
-            $mpdf->TOC_Entry($noun->title().' ('.$level.')', $level);
+            //add content to pdf
+            $mpdf->TOC_Entry($noun->title(), $level);
             $mpdf->WriteHTML($this->content($noun));
         } else {
             $mpdf->TOC_Entry($noun->title(), $level);
@@ -49,15 +78,29 @@ class PdfHelper extends \Digraph\Helpers\AbstractHelper
     public function pdf($noun)
     {
         $mpdf = $this->mpdf($noun);
+        //load from noun
         $this->mpdfNoun($mpdf, $noun);
+        $mpdf->SetHeaderByName($noun['dso.id'].'_right', 'O');
+        $mpdf->SetHeaderByName($noun['dso.id'].'_left', 'E');
+        $mpdf->SetFooterByName($noun['dso.id'].'_right', 'O');
+        $mpdf->SetFooterByName($noun['dso.id'].'_left', 'E');
         $mpdf->WriteHTML($this->content($noun));
+        //return output
         return $mpdf->output('', 'S');
     }
 
     protected function content($noun)
     {
         $out = '<h1>'.$noun->title().'</h1>';
-        $out .= $noun->body();
+        //column markup
+        if ($cols = $this->config($noun)['columns']) {
+            $out .= '<columns column-count="'.$cols['count'].'" />';
+            $out .= $noun->body();
+            $out .= '<columns column-count="0" />';
+        } else {
+            $out .= $noun->body();
+        }
+        //unpublished warning
         if (!$noun->isPublished()) {
             $out = "<div class=\"digraph-pdf-unpublished\">".
                    "<div class=\"digraph-pdf-unpublished-warning\">".
@@ -73,15 +116,32 @@ class PdfHelper extends \Digraph\Helpers\AbstractHelper
 
     protected function mpdfNoun(&$mpdf, $noun=null)
     {
-        if ($this->spread($noun)) {
-            $mpdf->mirrorMargins = 1;
-            $mpdf->setHTMLHeader($this->getTemplate('header_right', $noun), 'O');
-            $mpdf->setHTMLFooter($this->getTemplate('footer_right', $noun), 'O');
-            $mpdf->setHTMLHeader($this->getTemplate('header_left', $noun), 'E');
-            $mpdf->setHTMLFooter($this->getTemplate('footer_left', $noun), 'E');
+        if ($mpdf->mirrorMargins == 1) {
+            $mpdf->DefHTMLHeaderByName(
+                $noun['dso.id'].'_right',
+                $this->getTemplate('header_right', $noun)
+            );
+            $mpdf->DefHTMLHeaderByName(
+                $noun['dso.id'].'_left',
+                $this->getTemplate('header_left', $noun)
+            );
+            $mpdf->DefHTMLFooterByName(
+                $noun['dso.id'].'_right',
+                $this->getTemplate('footer_right', $noun)
+            );
+            $mpdf->DefHTMLFooterByName(
+                $noun['dso.id'].'_left',
+                $this->getTemplate('footer_left', $noun)
+            );
         } else {
-            $mpdf->setHTMLHeader($this->getTemplate('header', $noun), true);
-            $mpdf->setHTMLFooter($this->getTemplate('footer', $noun), true);
+            $mpdf->DefHTMLHeaderByName(
+                $noun['dso.id'].'_right',
+                $this->getTemplate('header', $noun)
+            );
+            $mpdf->DefHTMLFooterByName(
+                $noun['dso.id'].'_right',
+                $this->getTemplate('footer', $noun)
+            );
         }
     }
 
@@ -89,8 +149,12 @@ class PdfHelper extends \Digraph\Helpers\AbstractHelper
     {
         //instantiate Mpdf with config from CMS
         $mpdf = new Mpdf(
-            $this->cms->config['pdf.mpdf']
+            $this->config($noun)['mpdf']
         );
+        //set up for spread
+        if ($this->spread($noun)) {
+            $mpdf->mirrorMargins = 1;
+        }
         //set up print styles from CMS
         $mpdf->WriteHTML(
             $this->css($noun),
@@ -105,7 +169,7 @@ class PdfHelper extends \Digraph\Helpers\AbstractHelper
         if (isset($_GET['pdf_spread'])) {
             return boolval($_GET['pdf_spread']);
         }
-        return $this->cms->config['pdf.spread'];
+        return $this->config($noun)['spread'];
     }
 
     protected function css($noun=null)
@@ -116,6 +180,12 @@ class PdfHelper extends \Digraph\Helpers\AbstractHelper
     protected function getTemplate($name, $noun=null)
     {
         $t = $this->cms->helper('templates');
+        //check if config wants to override this template name
+        $config = $this->config($noun);
+        if ($config['templates.'.$name]) {
+            $name = $config['templates.'.$name];
+        }
+        //return rendered template
         return $t->render(
             $t->themeTemplate('pdf/'.$name.'.twig'),
             $this->templateFields($noun)
