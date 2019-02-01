fb2cc5b6449d3014f1d874c3292b703c849bd65e
Joby Elliott <elhober@unm.edu>
2019-02-01 08:45:59 -0700

    tidying block control panel

--- modules/digraph_block_types/routes/_blocks@all/display.php
+++ modules/digraph_block_types/routes/_blocks@all/display.php
@@ -1,9 +1,10 @@
 <?php
+$package->noCache();
 $search = $cms->factory()->search();
 $search->where('${dso.type} = :type');
 $areas = $search->execute(['type'=>'blockarea']);
 
-echo $cms->helper('urls')->parse('admin/add?type=blockarea')->html();
+echo $cms->helper('urls')->parse('_controlpanel/add?type=blockarea')->html();
 
 echo "<ul>";
 foreach ($areas as $area) {
