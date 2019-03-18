39ca55d7b5d17c127e7e62576b9888773f2c3b5c
Joby Elliott <elhober@unm.edu>
2019-03-18 09:22:54 -0600

    improved cron results output

--- routes/_cron/@all/display.php
+++ routes/_cron/@all/display.php
@@ -1,13 +1,19 @@
 <?php
-$package->makeMediaFile('cron.json');
-$package->noCache();
-$cron = [];
+$package->makeMediaFile('cron_result.json');
+$package['response.ttl'] = 3600;
+// $package->noCache();
+$CRON_OUTPUT = [
+    'started' => date('r'),
+    'modules' => []
+];
 
 //run cron hooks
 foreach ($this->helper('routing')->allHookFiles('_cron', 'cron.php') as $file) {
+    $cron = [];
     include $file['file'];
+    $CRON_OUTPUT['modules'][$file['module']] = $cron;
 }
 
-echo json_encode($cron, JSON_PRETTY_PRINT);
+echo json_encode($CRON_OUTPUT, JSON_PRETTY_PRINT);
 
 $package->saveLog('cron ran', 200, 'cron ran');
