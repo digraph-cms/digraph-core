9e2d3ca1879a2cefd1d34cfedc8f2b8a12834478
Joby Elliott <elhober@unm.edu>
2019-04-22 12:27:37 -0600

    skeleton of a logging munger
    this new mungertree might also be a way of running deferred code after 
    sending output, if the server supports it

---
+++ src/Logging/LogMunger.php
@@ -0,0 +1,16 @@
+<?php
+/* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
+namespace Digraph\Logging;
+
+use Digraph\Mungers\AbstractMunger;
+
+class LogMunger extends AbstractMunger
+{
+    protected function doMunge(&$package)
+    {
+    }
+
+    protected function doConstruct($name)
+    {
+    }
+}
