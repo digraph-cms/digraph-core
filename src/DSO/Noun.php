5994aabe2eae05e419478784e4d00ae871025287
Joby Elliott <joby@byjoby.com>
2019-04-28 20:06:40 -0600

    started reworking how locate/route steps work, to allow more flexibility in verb construction

--- src/DSO/Noun.php
+++ src/DSO/Noun.php
@@ -15,7 +15,14 @@ class Noun extends DSO implements NounInterface
     public function __construct(array $data = null, DSOFactoryInterface &$factory = null)
     {
         parent::__construct($data, $factory);
-        $this->merge($factory->cms()->config['defaultnoun']);
+        $defaults = $factory->cms()->config['defaultnoun.default'];
+        if (isset($factory->cms()->config['defaultnoun.'.$this['dso.type']])) {
+            $defaults = array_replace_recursive(
+                $defaults,
+                $factory->cms()->config['defaultnoun.'.$this['dso.type']]
+            );
+        }
+        $this->merge($defaults);
         $this->resetChanges();
     }
 
--- src/Mungers/Build/Route.php
+++ src/Mungers/Build/Route.php
@@ -10,24 +10,69 @@ class Route extends AbstractMunger
 
     protected function doMunge(&$package)
     {
-        $noun = $package['url.noun'];
-        $verb = $package['url.verb'];
+        $handlers = [];
+        $opts = $package['located-options'];
         $args = $package['url.args'];
-        if ($object = $package->noun()) {
-            $proper = true;
-            $noun = $package->noun()['dso.id'];
-            $type = $package->noun()['dso.type'];
-        } else {
-            $proper = false;
-            $type = $noun;
+        foreach ($opts as list($noun, $verb, $dso)) {
+            $dso = $package->cms()->read($dso, false);
+            if (!$verb) {
+                $verb = 'display';
+            }
+            //first check for file handler
+            $handler = $package->cms()->helper('routing')->file($dso['dso.type'], true, $verb.'.php');
+            if ($handler) {
+                $handlers[] = [
+                    $handler,
+                    $dso
+                ];
+            }
         }
-        //route with file handlers
-        $handler = $package->cms()->helper('routing')->file($type, $proper, $verb.'.php');
-        if (!$handler) {
+        //if no handler was found, try to find a common noun/verb pair
+        if (!$handlers) {
+            $pathString = trim($package->url()->pathString(), "/ \t\n\r\0\x0B");
+            $pathString = explode('/', $pathString);
+            if (count($pathString) <= 2) {
+                $type = $pathString[0];
+                $verb = isset($pathstring[1])?$pathString[1]:'display';
+                $handler = $package->cms()->helper('routing')->file($type, false, $verb.'.php');
+                if ($handler) {
+                    $handlers[] = [
+                        $handler,
+                        null
+                    ];
+                }
+            }
+        }
+        //put handlers into package
+        if (!$handlers) {
             $package->error(404, 'No route handler found');
-            return;
+        } elseif (count($handlers) === 1) {
+            $handler = array_pop($handlers);
+            $package['response.handler'] = $handler[0];
+            $package->noun($handler[1]);
+        } else {
+            echo "TODO: make 300 pages in Route munger";
+            var_dump($handlers);
+            exit();
         }
-        $package['response.handler'] = $handler;
+        // $noun = $package['url.noun'];
+        // $verb = $package['url.verb'];
+        // $args = $package['url.args'];
+        // if ($object = $package->noun()) {
+        //     $proper = true;
+        //     $noun = $package->noun()['dso.id'];
+        //     $type = $package->noun()['dso.type'];
+        // } else {
+        //     $proper = false;
+        //     $type = $noun;
+        // }
+        // //route with file handlers
+        // $handler = $package->cms()->helper('routing')->file($type, $proper, $verb.'.php');
+        // if (!$handler) {
+        //     $package->error(404, 'No route handler found');
+        //     return;
+        // }
+        // $package['response.handler'] = $handler;
     }
 
     protected function doConstruct($name)
