2a1eba989c117112bd8c32e5287df8acf10f9849
Joby Elliott <elhober@unm.edu>
2019-05-03 10:42:44 -0600

    fix for display hooks

--- routes/@any/display.php
+++ routes/@any/display.php
@@ -1,18 +1,18 @@
 <?php
-foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_first.php') as $file) {
+foreach ($this->helper('routing')->allHookFiles($package['noun.dso.type'], 'display_first.php') as $file) {
     include $file['file'];
 }
 
-foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_before.php') as $file) {
+foreach ($this->helper('routing')->allHookFiles($package['noun.dso.type'], 'display_before.php') as $file) {
     include $file['file'];
 }
 
 echo $package->noun()->body();
 
-foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_after.php') as $file) {
+foreach ($this->helper('routing')->allHookFiles($package['noun.dso.type'], 'display_after.php') as $file) {
     include $file['file'];
 }
 
-foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_last.php') as $file) {
+foreach ($this->helper('routing')->allHookFiles($package['noun.dso.type'], 'display_last.php') as $file) {
     include $file['file'];
 }
