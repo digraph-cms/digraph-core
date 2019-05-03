c26f6101f744c96ab85c9520866904a2ee036b2c
Joby Elliott <elhober@unm.edu>
2019-05-03 10:37:52 -0600

    adding some hooks to display verb

--- routes/@any/display.php
+++ routes/@any/display.php
@@ -1,2 +1,18 @@
 <?php
+foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_first.php') as $file) {
+    include $file['file'];
+}
+
+foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_before.php') as $file) {
+    include $file['file'];
+}
+
 echo $package->noun()->body();
+
+foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_after.php') as $file) {
+    include $file['file'];
+}
+
+foreach ($this->helper('routing')->allHookFiles($noun['dso.type'], 'display_last.php') as $file) {
+    include $file['file'];
+}
