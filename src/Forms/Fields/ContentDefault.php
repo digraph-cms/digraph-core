6e4b580f59c0027251ac13bb3f77af0553d6ab5f
Joby Elliott <elhober@unm.edu>
2019-06-05 10:50:26 -0600

    added ContentDefault field for simple non-configurable content fields

---
+++ src/Forms/Fields/ContentDefault.php
@@ -0,0 +1,42 @@
+<?php
+/* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
+namespace Digraph\Forms\Fields;
+
+use Formward\Fields\Container;
+use Formward\FieldInterface;
+use Digraph\CMS;
+use Formward\Fields\Checkbox;
+
+class ContentDefault extends Content
+{
+    public function default($default = null)
+    {
+        if (is_string($default)) {
+            $default = [
+                'text' => $default
+            ];
+        }
+        $out = parent::default($default);
+        $out['filter'] = 'default';
+        return $out;
+    }
+
+    public function value($value = null)
+    {
+        if (is_string($value)) {
+            $value = [
+                'text' => $value
+            ];
+        }
+        $out = parent::value($value);
+        $out['filter'] = 'default';
+        return $out;
+    }
+
+    public function __construct(string $label, string $name=null, FieldInterface $parent=null, CMS &$cms=null)
+    {
+        parent::__construct($label, $name, $parent, $cms);
+        unset($this['filter']);
+        unset($this['extra']);
+    }
+}
