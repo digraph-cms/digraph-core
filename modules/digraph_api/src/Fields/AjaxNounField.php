880fc6638e003d898fed4f9030d3811c475e03e8
Joby Elliott <elhober@unm.edu>
2019-07-18 11:38:28 -0600

    cleanup
    moving many things out to modules

--- modules/digraph_api/src/Fields/AjaxNounField.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
-namespace Digraph\Modules\digraph_api\Fields;
-
-class AjaxNounField extends AbstractAPIField
-{
-    const API_CMD = 'form_noun';
-}
