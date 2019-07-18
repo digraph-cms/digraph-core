931b6a92fe97cbc533df360a9b0e8f46a5e18ae3
Joby Elliott <elhober@unm.edu>
2019-07-18 10:52:02 -0600

    cleanup

--- src/FileStore/FileStoreHelper.php
+++ src/FileStore/FileStoreHelper.php
@@ -486,21 +486,3 @@ class FileStoreHelper extends AbstractHelper
         return $dir;
     }
 }
-
-function return_bytes($val)
-{
-    $val = trim($val);
-    $last = strtolower($val[strlen($val)-1]);
-    switch ($last) {
-        // The 'G' modifier is available since PHP 5.1.0
-        case 'g':
-            $val *= 1024;
-            // no break
-        case 'm':
-            $val *= 1024;
-            // no break
-        case 'k':
-            $val *= 1024;
-    }
-    return $val;
-}
