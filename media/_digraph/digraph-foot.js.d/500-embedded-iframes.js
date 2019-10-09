535ae332034dbb9d8ada9eeb090f96566b87a731
Joby Elliott <elhober@unm.edu>
2019-10-09 08:46:19 -0600

    iframe improvements

--- media/_digraph/digraph-foot.js.d/500-embedded-iframes.js
+++ media/_digraph/digraph-foot.js.d/500-embedded-iframes.js
@@ -5,21 +5,23 @@ $(() => {
         $iframes.each((i) => {
             var $iframe = $iframes.eq(i);
             var iframe = $iframe.get()[0];
+            var $contents = $iframe.contents();
+            var $body = $contents.find('body');
             //ensure iframe is wrapped
             if (!$iframe.parent().is('div.embedded-iframe')) {
-                $iframe.wrap('<div class="embedded-iframe" />');
+                $iframe.wrap('<div class="embedded-iframe loading" />');
                 $iframe.css('overflow','hidden');
                 $iframe.attr('scrolling','no');
             }
             //add iframe-embedded class
-            $iframe.contents().addClass('iframe-embedded');
+            $contents.addClass('iframe-embedded');
             //set up load/unload listeners
             iframe.onload = function(e) {
-                $(iframe).parent('div.embedded-iframe').removeClass('loading');
+                $(iframe).parent('div.embedded-iframe').addClass('loaded').removeClass('loading');
                 updateSingleFrame(iframe);
             };
             iframe.contentWindow.onunload = function(e) {
-                $(iframe).parent('div.embedded-iframe').addClass('loading');
+                $(iframe).parent('div.embedded-iframe').removeClass('loaded').addClass('loading');
             };
         });
     },100);
@@ -32,18 +34,21 @@ $(() => {
     };
     var updateSingleFrame = (iframe) => {
         var $iframe = $(iframe);
-        var $content = $iframe.contents();
+        var $contents = $iframe.contents();
         //set height if it isn't loading
-        if (!$iframe.is('.loading')) {
-            var height = $content.find('html').get(0).offsetHeight;
-            $iframe.animate({
-                height: height+'px'
-            }),'fast';
+        if (!$iframe.is('.loading') || ($iframe.is('.loading') && !$iframe.is('.loaded'))) {
+            var height = $contents.find('html').get(0).offsetHeight;
+            if (height && height != $contents.find('html').attr('data-ifheight')) {
+                $contents.find('html').attr('data-ifheight',height);
+                $iframe.animate({
+                    height: height+'px'
+                }),'fast';
+            }
         }
     };
     updateFrames();
     $(window).on('resize',updateFrames);
-    setInterval(updateFrames,250);
+    setInterval(updateFrames,100);
 });
 
 if (window!=window.top) { $('body').addClass('iframe-embedded'); }
