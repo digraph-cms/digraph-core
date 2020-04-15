9a50e08ca16f9254a28d6214f1a392a943f39e86
Joby Elliott <code@byjoby.com>
2020-04-15 09:12:13 -0600

    simpleMDE improvements

--- media/_digraph/digraph-foot.js.d/500-simplemde-integration.js
+++ media/_digraph/digraph-foot.js.d/500-simplemde-integration.js
@@ -1,41 +1,39 @@
 /**
  * Toggle SimpleMDE on content fields
  */
-document.addEventListener('DOMContentLoaded', function(e) {
-  var containers = document.querySelectorAll('.Form .Container.DigraphContent');
-  for (var i = 0; i < containers.length; i++) {
-    //add event listener to toggle SimpleMDE based on filter field
-    var container = containers[i];
-    var filterField = container.querySelectorAll('.Field.class-ContentFilter')[0];
-    var textArea = container.querySelectorAll('.Field.class-ContentTextarea')[0];
+$(() => {
+  $('.Form .Container.DigraphContent').each(function (e) {
+    var $container = $(this);
+    var $filter = $container.find('.Field.class-ContentFilter');
+    var $textArea = $container.find('.Field.class-ContentTextarea');
     var simpleMDE = null;
-    filterField.addEventListener('change', function(e) {
-      var name = this.options[this.selectedIndex].innerHTML;
-      if (name.match(/markdown/i)) {
-        if (!simpleMDE) {
-          //hide textarea and create SimpleMDE
-          textArea.classList.add('hidden');
-          simpleMDE = new SimpleMDE({
-            element: textArea,
-            autoDownloadFontAwesome: false,
-            spellChecker: false
-          });
-        }
-      } else {
-        if (simpleMDE) {
-          simpleMDE.toTextArea();
-          simpleMDE = null;
-          //unhide and dispatch change event to textarea so it will resize
-          textArea.classList.remove('hidden');
-          setTimeout(function() {
-            textArea.style.cssText = 'height:' + textArea.scrollHeight + 'px';
-          }, 0);
-        }
+    var updateFN = function (e) {
+      var markdown = !!$filter.find('[value="' + $filter.val() + '"]').text().match(/markdown/i);
+      if (markdown && !simpleMDE) {
+        // should be markdown, enable simpleMDE
+        simpleMDE = new SimpleMDE({
+          element: $textArea[0],
+          autoDownloadFontAwesome: false,
+          spellChecker: false
+        });
+        $textArea.addClass('hidden');
+      }
+      if (!markdown && simpleMDE) {
+        // should not be markdown, disable simpleMDE
+        simpleMDE.toTextArea();
+        simpleMDE = null;
+        $textArea.removeClass('hidden');
+        setTimeout(function () {
+          $textArea.height($textArea[0].scrollHeight);
+        }, 10);
+      }
+    };
+    $textArea.change(function (e) {
+      if (simpleMDE) {
+        simpleMDE.value($textArea.val());
       }
     });
-    //dispatch event immediately
-    var event = document.createEvent("HTMLEvents");
-    event.initEvent("change", true, true);
-    filterField.dispatchEvent(event);
-  }
+    $filter.change(updateFN);
+    updateFN();
+  });
 });
