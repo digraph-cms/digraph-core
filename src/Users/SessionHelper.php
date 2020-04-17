bd7ebd9e24f60bd86d2c09e8d9ee35add618e000
Joby Elliott <code@byjoby.com>
2020-04-17 10:50:40 -0600

    session data persistence URL token helper functions

--- src/Users/SessionHelper.php
+++ src/Users/SessionHelper.php
@@ -9,6 +9,27 @@ class SessionHelper extends AbstractHelper
 {
     protected $session;
 
+    public function getPersistedData($keep = false)
+    {
+        $data = $this->get('persistData.' . $_GET['pers_token']);
+        if (!$keep) {
+            $this->set('persistData.' . $_GET['pers_token'], false);
+        }
+        return $data;
+    }
+
+    public function persistData($data, $noun, $verb, $args = [])
+    {
+        $token = md5(serialize([$data, $noun, $verb, $args]));
+        $args['pers_token'] = $token;
+        $this->set('persistData.' . $token, $data);
+        return $this->cms->helper('urls')->url(
+            $noun,
+            $verb,
+            $args
+        );
+    }
+
     public function get($key)
     {
         return $this->session->get($key);
@@ -19,22 +40,22 @@ class SessionHelper extends AbstractHelper
         return $this->session->set($key, $value);
     }
 
-    public function getToken(string $name, int $ttl=3600*24) : string
+    public function getToken(string $name, int $ttl = 3600 * 24): string
     {
         return $this->session->getToken($name, $ttl);
     }
 
-    public function checkToken(string $name, $value, bool $keep=false) : bool
+    public function checkToken(string $name, $value, bool $keep = false): bool
     {
         return $this->session->checkToken($name, $value, $keep);
     }
 
-    public function userID(string $id = null) : ?string
+    public function userID(string $id = null): ?string
     {
         return $this->session->userID($id);
     }
 
-    public function userSID(string $id = null) : ?string
+    public function userSID(string $id = null): ?string
     {
         return $this->session->userSID($id);
     }
