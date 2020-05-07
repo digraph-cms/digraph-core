d8e32df33e6dee6b7383dde055864033a3d111eb
Joby Elliott <code@byjoby.com>
2020-05-07 11:22:03 -0600

    fix for unsafe markdown filter

---
+++ src/Filters/MarkdownFilterUnsafe.php
@@ -0,0 +1,20 @@
+<?php
+/* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
+namespace Digraph\Filters;
+
+/**
+ * Text filter converts multiple newlines into paragraph breaks. It should
+ * usually either be preceded by SanitizeFilter or followed by HTMLFilter with
+ * the P tag allowed.
+ */
+class MarkdownFilterUnsafe extends AbstractFilter
+{
+    public function filter(string $text, array $opts = []) : string
+    {
+        $text = str_replace('\\[', '\\\\\\[', $text);
+        $text = str_replace('\\]', '\\\\\\]', $text);
+        return \Parsedown::instance()
+            ->setUrlsLinked(false)
+            ->text($text);
+    }
+}
