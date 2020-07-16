44557eef2b55d7cdece7705e627be84f2257439b
Joby Elliott <code@byjoby.com>
2020-07-16 09:11:31 -0600

    use parsedown-extra instead of basic parsedown

--- src/Filters/MarkdownFilter.php
+++ src/Filters/MarkdownFilter.php
@@ -2,18 +2,13 @@
 /* Digraph Core | https://gitlab.com/byjoby/digraph-core | MIT License */
 namespace Digraph\Filters;
 
-/**
- * Text filter converts multiple newlines into paragraph breaks. It should
- * usually either be preceded by SanitizeFilter or followed by HTMLFilter with
- * the P tag allowed.
- */
 class MarkdownFilter extends AbstractFilter
 {
     public function filter(string $text, array $opts = []) : string
     {
         $text = str_replace('\\[', '\\\\\\[', $text);
         $text = str_replace('\\]', '\\\\\\]', $text);
-        return \Parsedown::instance()
+        return \ParsedownExtra::instance()
             ->setMarkupEscaped(true)
             ->setUrlsLinked(false)
             ->text($text);
