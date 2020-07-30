7680e246bed349d2af634d53832d2e41964322ff
Joby Elliott <code@byjoby.com>
2020-07-30 10:19:03 -0600

    permissions improvements, graph bug fix

--- src/Graph/GraphHelper.php
+++ src/Graph/GraphHelper.php
@@ -26,7 +26,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
         'depth' => -1,
         'onlyTypes' => [],
         'omitTypes' => [],
-        'sort' => 'tree'
+        'sort' => 'tree',
     ];
 
     /**
@@ -40,12 +40,12 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
      * @param boolean $reverse reverse order (searches up by default)
      * @return ?\Digraph\DSO\Noun
      */
-    public function nearest($start, $fnOrDSOType, $reverse=false)
+    public function nearest($start, $fnOrDSOType, $reverse = false)
     {
         $found = null;
         $this->traverse(
             $start,
-            function ($id) use ($found,$fnOrDSOType) {
+            function ($id) use (&$found, $fnOrDSOType) {
                 if ($noun = $this->cms->read($id)) {
                     if (is_callable($fnOrDSOType)) {
                         if ($fnOrDSOType($noun)) {
@@ -71,7 +71,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     Wrapper for childIDs that actually loads all the Nouns from the database.
     Maintains breadth-first order.
      */
-    public function children($id, $type=null, $depth=1, $order=null)
+    public function children($id, $type = null, $depth = 1, $order = null)
     {
         return $this->sqlResults($this->childIDs($id, $type, $depth), $order);
     }
@@ -80,7 +80,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     Wrapper for parentIDs that actually loads all the Nouns from the database.
     Maintains breadth-first order.
      */
-    public function parents($id, $type=null, $depth=1, $order=null)
+    public function parents($id, $type = null, $depth = 1, $order = null)
     {
         return $this->sqlResults($this->parentIDs($id, $type, $depth), $order);
     }
@@ -89,10 +89,10 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     Return all children of a given ID, optionally up to a given depth and/or of
     a specific edge type. Returns the IDs only.
      */
-    public function childIDs($id, $type=null, $depth=1)
+    public function childIDs($id, $type = null, $depth = 1)
     {
         $out = $this->traverse($id, null, $type, $depth);
-        array_shift($out);//shift first item off to not include root of search
+        array_shift($out); //shift first item off to not include root of search
         return $out;
     }
 
@@ -100,10 +100,10 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     Return all parents of a given ID, optionally up to a given depth and/or of
     a specific edge type. Returns the IDs only.
      */
-    public function parentIDs($id, $type=null, $depth=1)
+    public function parentIDs($id, $type = null, $depth = 1)
     {
         $out = $this->traverse($id, null, $type, $depth, true);
-        array_shift($out);//shift first item off to not include root of search
+        array_shift($out); //shift first item off to not include root of search
         return $out;
     }
 
@@ -111,7 +111,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     Wrapper for routeIDs that actually loads all the Nouns from the database.
     Maintains breadth-first order.
      */
-    public function route($start, $end, $reverse=false)
+    public function route($start, $end, $reverse = false)
     {
         return $this->sqlResults($this->routeIDs($start, $end, $reverse));
     }
@@ -119,7 +119,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
     /*
     Get an array of IDs that form a route between a given start and end point.
      */
-    public function routeIDs($start, $end) : ?array
+    public function routeIDs($start, $end): ?array
     {
         /*
         It's noteworthy that this method works in a slightly unintuitive way.
@@ -152,7 +152,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
         return null;
     }
 
-    protected function sqlResults($ids=[], $order=null)
+    protected function sqlResults($ids = [], $order = null)
     {
         if ($s = $this->sqlSearch($ids)) {
             if ($order === null) {
@@ -181,7 +181,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
      * Creates a Destructr search object set up to return a given list of IDs,
      * optionally limiting to and omitting by type
      */
-    protected function sqlSearch($ids=[])
+    protected function sqlSearch($ids = [])
     {
         //sanitize IDs
         $ids = $this->regexFilter($ids, '/^[a-z0-9]+$/');
@@ -193,7 +193,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
         $search = $this->cms->factory()->search();
         //set up where clause
         $ids = array_map([$search, 'quote'], $ids);
-        $search->where('${dso.id} in ('.implode(",", $ids).')');
+        $search->where('${dso.id} in (' . implode(",", $ids) . ')');
         //return search object
         return $search;
     }
@@ -224,17 +224,17 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
      *
      * If no callback is provided, each item will simply be the ID of the noun.
      */
-    public function traverse($start, $fn=null, $type=null, $maxDepth=-1, $reverse=false)
+    public function traverse($start, $fn = null, $type = null, $maxDepth = -1, $reverse = false)
     {
-        $queue = [[0,$start,null]];
+        $queue = [[0, $start, null]];
         $results = [];
         while ($queue) {
             list($depth, $id, $last) = array_shift($queue);
             if (!$id || isset($results[$id])) {
                 continue;
             }
-            $r = $fn?$fn($id, $depth, $last):$id;
-            $results[$id] = $r?$r:false;
+            $r = $fn ? $fn($id, $depth, $last) : $id;
+            $results[$id] = $r ? $r : false;
             //if result is false, abort traverse
             //this means callback should return null if it doesn't want to abort
             if ($r === false) {
@@ -247,7 +247,7 @@ class GraphHelper extends \Digraph\Helpers\AbstractHelper
                     $new = $this->cms->helper('edges')->children($id, $type, true);
                 }
                 foreach ($new as $n) {
-                    $queue[] = [$depth+1,$n,$id];
+                    $queue[] = [$depth + 1, $n, $id];
                 }
             }
         }
--- src/Permissions/PermissionsHelper.php
+++ src/Permissions/PermissionsHelper.php
@@ -2,39 +2,63 @@
 /* Digraph Core | https://github.com/jobyone/digraph-core | MIT License */
 namespace Digraph\Permissions;
 
+use Digraph\DSO\Noun;
 use Digraph\Helpers\AbstractHelper;
 use Digraph\Urls\Url;
-use Digraph\DSO\Noun;
 
 class PermissionsHelper extends AbstractHelper
 {
+    protected $urlCache = [];
+
     /**
      * Check whether a user is allowed to access a particular URL. Will also
      * run a special check on add permissions if this is a proper noun's "add"
      * verb. The permissions this method checks are controlled through the
      * config permissions.url
+     *
+     * Returns true/false for a definitive result, null for undefined (which
+     * should probably generally be treated as false).
+     *
+     * @param Url $url
+     * @param string $userID
+     * @return boolean|null
      */
-    public function checkUrl(Url $url, string $userID = null) : bool
+    public function checkUrl(Url $url, string $userID = null): ?bool
     {
-        $path = '';
+        $id = md5(serialize([$url,$userID]));
+        if (!isset($this->urlCache[$id])) {
+            $this->urlCache[$id] = $this->doCheckUrl($url,$userID);
+        }
+        return $this->urlCache[$id];
+    }
+
+    public function doCheckUrl(Url $url, string $userID = null): ?bool
+    {
+        $paths = [];
         $noun = $this->cms->helper('urls')->noun($url);
         if ($noun) {
-            //pass off to checkAddPermissions if verb is "add"
-            if ($url['verb'] == 'add') {
-                return $this->checkAddPermissions($noun, $url['args']['type'], $userID);
-            }
-            //use dso type as start of path
-            $path = $noun['dso.type'];
-            //check for permissions based on specific noun
-            if ($this->check($noun['dso.id'].'/'.$url['verb'])) {
-                return true;
-            }
+            // check based on specific noun
+            $paths[] = $noun['dso.id']. '/' . $url['verb'];
+            // check based on dso type
+            $paths[] = $noun['dso.type']. '/' . $url['verb'];
         } else {
-            //use url noun
-            $path = $url['noun'];
+            // check using url type
+            $paths[] = $url['noun']. '/' . $url['verb'];
         }
-        $path .= '/'.$url['verb'];
-        return $this->check($path, 'url', $userID);
+        // check all paths in decreasing order of specificity
+        foreach ($paths as $path) {
+            $result = $this->check($path,'url',$userID);
+            // returns the first non-null result we get
+            if ($result !== null) {
+                // if there is a noun and the verb is add, we also need to checkAddPermissions
+                if ($result && $noun && $url['verb'] == 'add') {
+                    return $this->checkAddPermissions($noun, $url['args']['type'], $userID);
+                }
+                // otherwise we can just return the result
+                return $result;
+            }
+        }
+        return null;
     }
 
     /**
@@ -47,7 +71,7 @@ class PermissionsHelper extends AbstractHelper
      * to the parent type, which is controlled by
      * permissions.add.[parent type]/[child type]
      */
-    public function checkAddPermissions($parentOrType, $type, string $userID=null) : bool
+    public function checkAddPermissions($parentOrType, $type, string $userID = null): bool
     {
         if ($parentOrType instanceof Noun) {
             $parentType = $parentOrType['dso.type'];
@@ -55,12 +79,21 @@ class PermissionsHelper extends AbstractHelper
             $parentType = $parentOrType;
         }
         return
-            //user must have all the following permissions
-            $this->check($parentType.'/add', 'url', $userID) &&//url add verb permission
-            $this->check($parentType.'/'.$type, 'add', $userID);//add type under parent type
+        //user must have all the following permissions
+        $this->check($parentType . '/add', 'url', $userID) && //url add verb permission
+        $this->check($parentType . '/' . $type, 'add', $userID); //add type under parent type
     }
 
-    public function check(string $path, string $category='url', string $userID = null) : bool
+    /**
+     * Check a given path/category with a given URL. Returns true/false for
+     * definitive results, null if no matching permissions were returned.
+     *
+     * @param string $path
+     * @param string $category
+     * @param string $userID
+     * @return boolean|null
+     */
+    public function check(string $path, string $category = 'url', string $userID = null): ?bool
     {
         $allow = false;
         $rules = @$this->cms->config['permissions'][$category];
@@ -80,17 +113,18 @@ class PermissionsHelper extends AbstractHelper
             foreach ($path as $i) {
                 $n++;
                 if (!$matchingKeys) {
-                    $matchingKeys = ['*',$i];
+                    $matchingKeys = ['*', $i];
                 } else {
                     foreach ($matchingKeys as $k) {
-                        $matchingKeys[] = $k.'/*';
-                        $matchingKeys[] = $k.'/'.$i;
+                        $matchingKeys[] = $k . '/*';
+                        $matchingKeys[] = $k . '/' . $i;
                     }
                     $matchingKeys = array_unique($matchingKeys);
                 }
             }
             foreach ($matchingKeys as $key) {
                 if (isset($rules[$key])) {
+                    // var_dump($key);
                     foreach ($rules[$key] as $rule) {
                         $new = $this->checkRule($rule, $userID, $groups);
                         if ($new !== null) {
