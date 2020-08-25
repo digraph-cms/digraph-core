6a19faf5d8ea825fdb14861d9a1854bb750d2043
Joby Elliott <code@byjoby.com>
2020-08-25 08:44:12 -0600

    code highlighting performance fix

--- src/Filters/CodeHighlightFilter.php
+++ src/Filters/CodeHighlightFilter.php
@@ -6,12 +6,21 @@ use Highlight\Highlighter;
 
 class CodeHighlightFilter extends AbstractFilter
 {
-    public function filter(string $text, array $opts = []) : string
+    public function filter(string $text, array $opts = []): string
     {
-        $h = new Highlighter;
+        // short circuit if there's definitely no code tag
+        if (stripos($text, '<code') === false) {
+            return $text;
+        }
+        // do highlighting
+        $h = false;
         $text = preg_replace_callback(
             "/<code( class=\"language-(.+?)\")?>(.+?)<\/code>/ims",
-            function ($matches) use ($h) {
+            function ($matches) use (&$h) {
+                // instantiate if highlight is necessary
+                if (!$h) {
+                    $h = new Highlighter;
+                }
                 //do highlighting
                 $lang = @$matches[2];
                 $code = trim($matches[3]);
@@ -30,16 +39,16 @@ class CodeHighlightFilter extends AbstractFilter
                     $code = preg_split('/(\r\n|\n|\r)/', $code);
                     $code = array_map(
                         function ($e) {
-                            return '<span class="code-highlighted-line">'.$e.'</span>';
+                            return '<span class="code-highlighted-line">' . $e . '</span>';
                         },
                         $code
                     );
                     $code = implode(PHP_EOL, $code);
                     //return as a DIV
-                    return '<div class="code-highlighted language-'.$highlighted->language.'">'.$code.'</div>';
+                    return '<div class="code-highlighted language-' . $highlighted->language . '">' . $code . '</div>';
                 }
                 //return as a SPAN
-                return '<span class="code-highlighted language-'.$highlighted->language.'">'.$code.'</span>';
+                return '<span class="code-highlighted language-' . $highlighted->language . '">' . $code . '</span>';
             },
             $text
         );
