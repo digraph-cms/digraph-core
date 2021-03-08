914434e675f7ba89716d046909801cbae38508fc
Joby Elliott <jelliott@cs.unm.edu>
2021-03-08 12:05:03 -0700

    improved log readability

--- routes/_logging@all/display.php
+++ routes/_logging@all/display.php
@@ -7,34 +7,31 @@ $s = $cms->helper('strings');
 $search = $this->factory('logging')->search();
 $search->order('${dso.modified.date} DESC');
 
-$classes = [
-    'INFO' => '',
-    'DEBUG' => '',
-    'NOTICE' => 'highlighted-notice',
-    'WARNING' => 'highlighted-warning',
-    'ERROR' => 'highlighted-error',
-    'CRITICAL' => 'highlighted-error',
-    'EMERGENCY' => 'highlighted-error',
-];
-
 echo $p->paginate(
     $search->count(),
     $package,
     'page',
     20,
-    function ($start, $end) use ($classes, $search) {
-        $out = '';
-        $out .= "<table>";
-        $out .= "<tr><th>Log</th><th>Count</th><th>Path</th></tr>";
+    function ($start, $end) use ($search, $cms) {
+        $date = null;
         $search = clone $search;
         $search->limit($end - $start);
         $search->offset($start - 1);
+        $out = '';
         foreach ($search->execute() as $log) {
-            $out .= "<tr class='" . @$classes[$log->level()] . "'>";
-            $out .= "<td><a href='" . $log->url() . "'>" . $log->name() . "</a></td>";
-            $out .= "<td>" . $log['count'] . "</td>";
-            $out .= "<td style='width:25%;'><div style='max-width:10em;overflow:hidden;white-space:nowrap;'>" . $log['package.request.url'] . "</div></td>";
-            $out .= "</tr>";
+            $mDate = $cms->helper('strings')->date($log['dso.modified.date']);
+            if ($mDate != $date) {
+                $date = $mDate;
+                $out .= "<h2>$date</h2>";
+            }
+            $out .= "<div class='digraph-card'>";
+            $out .= "<code><a href='".$log->url()."'>".$log->name()."</a></code>";
+            $out .= "</div>";
+            // $out .= "<tr class='" . @$classes[$log->level()] . "'>";
+            // $out .= "<td style='max-width:75%;overflow:hidden;'><a href='" . $log->url() . "'>" . $log->name() . "</a></td>";
+            // $out .= "<td>" . $log['count'] . "</td>";
+            // $out .= "<td style='width:25%;'><div style='max-width:10em;overflow:hidden;white-space:nowrap;'>" . $log['package.request.url'] . "</div></td>";
+            // $out .= "</tr>";
         }
         $out .= "</table>";
         return $out;
