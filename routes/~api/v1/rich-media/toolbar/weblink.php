2fbd51ac5ce9e51feb0a37fae09ff33ec7666a80
Joby Elliott <code@byjoby.com>
2022-01-18 15:20:45 -0700

    weblink toolbar interface works

--- routes/~api/v1/rich-media/toolbar/weblink.php
+++ routes/~api/v1/rich-media/toolbar/weblink.php
@@ -1,34 +1,46 @@
 <?php
 
 use DigraphCMS\Digraph;
-use DigraphCMS\HTML\DIV;
-use DigraphCMS\HTML\Forms\FORM;
-use DigraphCMS\HTML\Forms\FormWrapper;
+use DigraphCMS\HTML\A;
 use DigraphCMS\HTML\Forms\INPUT;
 use DigraphCMS\UI\Toolbars\ToolbarLink;
+use DigraphCMS\UI\Toolbars\ToolbarSeparator;
+use DigraphCMS\UI\Toolbars\ToolbarSpacer;
 
 echo new ToolbarLink('', 'web', null, null);
-$wrapper = (new FormWrapper())->setID(Digraph::uuid())
-    ->setStyle('width', '100%')
-    ->setStyle('display', 'contents');
-$wrapper->button()->setText('Insert');
-$input = (new INPUT(Digraph::uuid()))
+echo $input = (new INPUT(Digraph::uuid()))
     ->addClass('navigation-frame__autofocus');
-$wrapper->addChild($input);
+echo $submit = (new A())
+    ->setID(Digraph::uuid())
+    ->addClass('button')
+    ->addClass('button--inverted')
+    ->addChild('Insert');
 echo $wrapper;
 
 ?>
-<!-- <script>
+<script>
     (() => {
-        var ac = document.getElementById('<?php echo $wrapper->id(); ?>');
-        // select value and reset toolbar
-        ac.addEventListener('autocomplete-select', (e) => {
-            ac.dispatchEvent(Digraph.RichContent.insertTagEvent('link', {
-                _: e.autocompleteValue
-            }));
-            ac.dispatchEvent(new Event('navigation-frame-reset', {
+        var input = document.getElementById('<?php echo $input->id(); ?>');
+        var submit = document.getElementById('<?php echo $submit->id(); ?>');
+        var insert_fn = () => {
+            input.dispatchEvent(Digraph.RichContent.insertEvent(
+                '[{content}]({0})'.format(input.value),
+                '[{0}]({1})'.format(
+                    input.value.replace(/^(https?:)?\/\//, ''),
+                    input.value
+                )
+            ));
+            input.dispatchEvent(new Event('navigation-frame-reset', {
                 bubbles: true
             }));
+        };
+        input.addEventListener('keydown', (e) => {
+            if (e.key == 'Enter') {
+                insert_fn();
+            }
+        });
+        submit.addEventListener('click', (e) => {
+            insert_fn();
         });
     })();
-</script> -->
+</script>
