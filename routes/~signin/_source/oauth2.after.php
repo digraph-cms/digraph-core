2b85f175ca19fa0a5fe773baa701558712bcf7b4
Joby Elliott <code@byjoby.com>
2022-03-10 12:08:15 -0700

    small tweaks

--- routes/~signin/_source/oauth2.after.php
+++ routes/~signin/_source/oauth2.after.php
@@ -9,7 +9,11 @@ $user = Users::current();
 foreach (Context::data('oauth_resource_owner')->toArray() as $key => $value) {
     if (stripos($key, 'email') !== false && filter_var($value, FILTER_VALIDATE_EMAIL)) {
         $count = count($user->emails());
-        $user->addEmail($value, 'Added from ' . Config::get("user_sources.oauth2.providers." . Context::arg('_provider') . ".name") . ' via OAuth signin');
+        $user->addEmail(
+            $value,
+            'Added from ' . Config::get("user_sources.oauth2.providers." . Context::arg('_provider') . ".name") . ' via OAuth signin',
+            true
+        );
     }
 }
 $user->update();
