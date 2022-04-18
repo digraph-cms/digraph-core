f15905f2adf2804441859341f2c9b50d1a597f46
Joby Elliott <code@byjoby.com>
2022-04-18 12:47:52 -0600

    another small warning squashed

--- src/HTML/Forms/SELECT.php
+++ src/HTML/Forms/SELECT.php
@@ -10,6 +10,7 @@ class SELECT extends Tag implements InputInterface
     protected $tag = 'select';
     protected $options = [];
     protected $required = false;
+    protected $requiredMessage;
     protected $value;
 
     /** @var FormWrapper|null */
