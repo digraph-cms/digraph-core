ca13c7f757fd865288b79a3f8d281aff94062f1e
Joby Elliott <code@byjoby.com>
2022-07-29 16:56:09 -0600

    theme fix, to load the script inside the navigation frame

--- routes/~api/v1/usermenu/theme.php.action.php
+++ routes/~api/v1/usermenu/theme.php.action.php
@@ -46,12 +46,12 @@ $colorblind = new ButtonMenu('Colorblind mode', [
 ]);
 
 echo "<div class='theme-menu navigation-frame navigation-frame--stateless' id='theme-menu'>";
+
 echo "<h1>Color settings</h1>";
 echo "<h2>Dark/light mode</h2>";
 echo $mode;
 echo "<h2>Colorblind mode</h2>";
 echo $colorblind;
-echo "</div>";
 
 // also generate a script that sets the appropriate body classes on load
 echo "<script>";
@@ -71,3 +71,5 @@ if (Theme::colorblindMode()) {
     echo "document.body.classList.remove('colors--colorblind');";
 }
 echo "</script>";
+
+echo "</div>";
