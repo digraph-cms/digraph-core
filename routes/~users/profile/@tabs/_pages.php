8d0fcf5cf27650d323eb4e3aa38a0c05b63e29d1
Joby Elliott <code@byjoby.com>
2022-10-28 09:16:34 -0600

    page filtering bug fix

--- routes/~users/profile/@tabs/_pages.php
+++ routes/~users/profile/@tabs/_pages.php
@@ -4,8 +4,9 @@ use DigraphCMS\Content\Pages;
 use DigraphCMS\UI\Pagination\PageTable;
 
 $pages = Pages::select()
-    ->where('created_by = ? OR updated_by = ?', [$user->uuid(), $user->uuid()])
+    ->where('(created_by = ? OR updated_by = ?)', [$user->uuid(), $user->uuid()])
     ->order('updated DESC');
+
 if ($pages->count()) {
     echo "<h1>Pages created/modified</h1>";
     echo "<p>The following pages were created or last modified by this user.</p>";
