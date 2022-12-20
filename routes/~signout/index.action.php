2b19fba9b20d6deff7faea5ee5197bd7fa903c1e
Joby Elliott <code@byjoby.com>
2022-12-20 12:55:50 -0700

    fix for logout bounce permissions check

--- routes/~signout/index.action.php
+++ routes/~signout/index.action.php
@@ -5,13 +5,14 @@ use DigraphCMS\Context;
 use DigraphCMS\Session\Session;
 use DigraphCMS\URL\URL;
 use DigraphCMS\Users\Permissions;
+use DigraphCMS\Users\Users;
 
 Session::deauthenticate('Used signout link');
 
 $bounce = Context::arg('_bounce');
 if ($bounce) {
     $bounce = new URL($bounce);
-    if (!Permissions::url($bounce)) {
+    if (!Permissions::url($bounce, Users::guest())) {
         $bounce = new URL('/');
     }
 } else {
