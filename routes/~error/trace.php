d35320b70232add64ba4cbe30e44b59baf2ec3ee
Joby Elliott <code@byjoby.com>
2023-01-03 12:02:49 -0700

    trace fixes

--- routes/~error/trace.php
+++ routes/~error/trace.php
@@ -18,40 +18,44 @@ if (Config::get('errors.display_trace')) {
     echo "<section class='stack-trace'>";
     echo "<h1>Stack trace:<br>" . get_class($thrown) . "</h1>";
     echo "<div class='error'>";
-    if ($thrown instanceof Exception) {
+    if (method_exists($thrown, 'getMessage')) {
         echo "<strong>" . htmlentities($thrown->getMessage()) . "</strong>";
     }
     echo '<br>';
-    echo $trim($thrown->getFile()) . ':' . $thrown->getLine();
+    if (method_exists($thrown, 'getFile') && method_exists($thrown, 'getLine')) {
+        echo $trim($thrown->getFile()) . ':' . $thrown->getLine();
+    }
     echo "</div>";
-    echo "<div class='stack-trace'>";
-    foreach ($thrown->getTrace() as $t) {
-        echo "<div>";
-        if (@$t['file']) {
-            echo "<strong>" . htmlentities($trim(@$t['file'])) . ":" . @$t['line'] . "</strong><br>";
-        }
-        echo "<em>" . @$t['class'] . @$t['type'] . @$t['function'] . '()</em>';
-        if (@$t['args']) {
-            echo "<div class='trace-args'>";
-            foreach ($t['args'] as $arg) {
-                $arg = htmlentities(print_r($arg, true));
-                if (strpos($arg, "\n")) {
-                    $id = crc32(serialize([@$t['class'], @$t['type'], @$t['function'], $arg]));
-                    echo "<div class=\"collapsible-multiline\" id=\"$id\">";
-                    echo "<div id=\"$id-collapsed\">";
-                    echo "<a class=\"expand\" href=\"#$id\">+</a>";
-                    echo "<a class=\"collapse\" href=\"#$id-collapsed\">-</a>";
-                    echo '&nbsp;' . $arg;
-                    echo "</div>";
-                    echo "</div>";
-                } else {
-                    echo "<div>$arg</div>";
+    if (method_exists($thrown, 'getTrace')) {
+        echo "<div class='stack-trace'>";
+        foreach ($thrown->getTrace() as $t) {
+            echo "<div>";
+            if (@$t['file']) {
+                echo "<strong>" . htmlentities($trim(@$t['file'])) . ":" . @$t['line'] . "</strong><br>";
+            }
+            echo "<em>" . @$t['class'] . @$t['type'] . @$t['function'] . '()</em>';
+            if (@$t['args']) {
+                echo "<div class='trace-args'>";
+                foreach ($t['args'] as $arg) {
+                    $arg = htmlentities(print_r($arg, true));
+                    if (strpos($arg, "\n")) {
+                        $id = crc32(serialize([@$t['class'], @$t['type'], @$t['function'], $arg]));
+                        echo "<div class=\"collapsible-multiline\" id=\"$id\">";
+                        echo "<div id=\"$id-collapsed\">";
+                        echo "<a class=\"expand\" href=\"#$id\">+</a>";
+                        echo "<a class=\"collapse\" href=\"#$id-collapsed\">-</a>";
+                        echo '&nbsp;' . $arg;
+                        echo "</div>";
+                        echo "</div>";
+                    } else {
+                        echo "<div>$arg</div>";
+                    }
                 }
+                echo "</div>";
             }
             echo "</div>";
         }
         echo "</div>";
     }
-    echo "</div>";
     echo "</section>";
 }
