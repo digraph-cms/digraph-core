820a987d93421ccc663995349ba9f9e9440c7afb
Joby Elliott <code@byjoby.com>
2023-02-08 13:45:05 -0700

    fix deprecated call

--- src/HTML/Forms/Phone.php
+++ src/HTML/Forms/Phone.php
@@ -17,7 +17,7 @@ class Phone extends INPUT
 
     public function value($useDefault = false)
     {
-        $value = preg_replace('/[^0-9]/', '', parent::value($useDefault));
+        $value = preg_replace('/[^0-9]/', '', parent::value($useDefault) ?? '');
         if (strlen($value) == 7) return sprintf('%s-%s', substr($value, 0, 3), substr($value, 3, 4));
         elseif (strlen($value) == 10) return sprintf('(%s) %s-%s', substr($value, 0, 3), substr($value, 3, 3), substr($value, 6, 4));
         else return $value;
