6f79fc3b9b2d581a1ddd21dccfbebc8f5ebda9ea
Joby Elliott <code@byjoby.com>
2023-02-22 09:07:00 -0700

    fix for phone number field static analysis

--- src/HTML/Forms/Phone.php
+++ src/HTML/Forms/Phone.php
@@ -17,10 +17,11 @@ class Phone extends INPUT
 
     public function value($useDefault = false)
     {
-        $value = preg_replace('/[^0-9]/', '', parent::value($useDefault) ?? '');
+        /** @var string */
+        $value = preg_replace('/[^0-9]/', '', parent::value($useDefault) ?? '') ?? '';
         if (strlen($value) == 7) return sprintf('%s-%s', substr($value, 0, 3), substr($value, 3, 4));
         elseif (strlen($value) == 10) return sprintf('(%s) %s-%s', substr($value, 0, 3), substr($value, 3, 3), substr($value, 6, 4));
-        else return $value;
+        else return $value ? $value : null;
     }
 
     public function attributes(): array
