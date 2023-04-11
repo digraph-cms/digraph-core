e4ad6b6f73ff2ac303d8228cc4c7c95d466962af
Joby Elliott <code@byjoby.com>
2023-04-11 12:28:36 -0600

    migration fix

--- phinx/migrations/20221005164420_no_save_blocked_emails.php
+++ phinx/migrations/20221005164420_no_save_blocked_emails.php
@@ -8,6 +8,7 @@ final class NoSaveBlockedEmails extends AbstractMigration
     public function change(): void
     {
         $this->table('email')
+            ->removeIndex('blocked')
             ->removeColumn('blocked')
             ->save();
     }
