13618f5f30226d097fe2490f69f804073ff5018f
Joby Elliott <code@byjoby.com>
2023-06-27 09:43:26 -0600

    removing extraneous logging

--- media/scripts/draggable-content.js
+++ media/scripts/draggable-content.js
@@ -1,5 +1,4 @@
 document.addEventListener('dragstart', (e) => {
-  console.log(e);
   if ('dragContent' in e.target.dataset)
     e.dataTransfer.setData('text/plain',e.target.dataset.dragContent)
 });
