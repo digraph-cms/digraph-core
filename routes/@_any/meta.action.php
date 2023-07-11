e60c2c0fa39491a26bc3716e948a6668dc98119d
Joby Elliott <code@byjoby.com>
2023-07-11 12:27:27 -0600

    update to meta tool

--- routes/@_any/meta.action.php
+++ routes/@_any/meta.action.php
@@ -1,6 +1,11 @@
 <?php
 
+use DigraphCMS\CodeMirror\YamlArrayInput;
 use DigraphCMS\Context;
+use DigraphCMS\HTML\Forms\Field;
+use DigraphCMS\HTML\Forms\FormWrapper;
+use DigraphCMS\HTTP\RefreshException;
+use DigraphCMS\UI\Notifications;
 
 echo "<table>";
 foreach (Context::page()->metadata() as $k => $v) {
@@ -10,8 +15,30 @@ foreach (Context::page()->metadata() as $k => $v) {
                 return "<li>$v</li>";
             },
             $v
-        )) . '</ul>';
+        )
+        ) . '</ul>';
     }
     printf('<tr><th>%s</th><td>%s</td></tr>', $k, $v);
 }
 echo "</table>";
+
+if (!Context::page()->isAdmin()) return;
+echo '<div class="navigation-frame navigation-frame--stateless" id="meta-raw-data-editor">';
+echo '<h2>Raw data</h2>';
+Notifications::printWarning('<strong>Warning:</strong> this tool can permanently delete data, and may put pages into unknown states that produce errors. Use with caution.');
+$form = new FormWrapper();
+$input = (new YamlArrayInput())
+    ->setDefault(Context::page()->get(null));
+$field = (new Field('Data', $input))
+    ->setRequired(true)
+    ->addForm($form);
+if ($form->ready()) {
+    Context::page()
+        ->set(null,[])
+        ->set(null,$input->value())
+        ->update();
+    Notifications::flashConfirmation('Saved page data');
+    throw new RefreshException;
+}
+echo $form;
+echo '</div>';
