982f82e34cbbaf7623580e1eaa27588eea27f87d
Joby Elliott <code@byjoby.com>
2023-10-11 16:09:59 +0000

    trying a different locking mechanism

--- src/Cache/Locking.php
+++ src/Cache/Locking.php
@@ -2,11 +2,14 @@
 
 namespace DigraphCMS\Cache;
 
+use DigraphCMS\Config;
 use DigraphCMS\DB\DB;
+use Symfony\Component\Lock\Exception\LockAcquiringException;
 use Symfony\Component\Lock\Exception\LockConflictedException;
 use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Lock\Store\PdoStore;
 use Symfony\Component\Lock\SharedLockInterface;
+use Symfony\Component\Lock\Store\FlockStore;
 
 class Locking
 {
@@ -22,6 +25,9 @@ class Locking
                 static::$locks[$name] = $lock;
                 return true;
             } catch (LockConflictedException) {
+                if ($blocking) usleep(random_int(0, 100));
+                else return false;
+            } catch (LockAcquiringException) {
                 if ($blocking) usleep(random_int(0, 100));
                 else return false;
             }
@@ -40,7 +46,8 @@ class Locking
     {
         static $store;
         static $factory;
-        $store = $store ?? new PdoStore(DB::pdo());
+        // $store = $store ?? new PdoStore(DB::pdo());
+        $store = $store ?? new FlockStore(Config::cachePath() . '/locking');
         $factory = $factory ?? new LockFactory($store);
         return $factory;
     }
