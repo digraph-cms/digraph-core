250d91b2c49beba9c3df6947a23f8844478e9964
Joby Elliott <code@byjoby.com>
2023-12-18 10:05:37 -0700

    use built-in storage createTable method

--- phinx/migrations/20231218164549_symfony_locking_table.php
+++ phinx/migrations/20231218164549_symfony_locking_table.php
@@ -2,21 +2,15 @@
 
 declare(strict_types=1);
 
+use DigraphCMS\DB\DB;
 use Phinx\Migration\AbstractMigration;
+use Symfony\Component\Lock\Store\PdoStore;
 
 final class SymfonyLockingTable extends AbstractMigration
 {
     public function change(): void
     {
-        $this->table('lock_keys', [
-            'id' => false,
-            'primary_key' => ['key_id'],
-            // 'encoding'  => 'utf8',
-            // 'collation' => 'utf8_mb4_bin',
-        ])
-            ->addColumn('key_id', 'string', ['length' => 64, 'null' => false])
-            ->addcolumn('key_token', 'string', ['length' => 44, 'null' => false])
-            ->addColumn('key_expiration', 'integer', ['signed' => false, 'length' => 10, 'null' => false])
-            ->create();
+        (new PdoStore(DB::pdo()))
+            ->createTable();
     }
 }
