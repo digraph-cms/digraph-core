5baf23eb1db1f6cf1d373ad9e7b86d4a0f10e90a
Joby Elliott <jelliott@cs.unm.edu>
2024-03-29 10:27:37 -0600

    possible bug fix

--- src/UI/ButtonMenus/ButtonMenu.php
+++ src/UI/ButtonMenus/ButtonMenu.php
@@ -2,18 +2,19 @@
 
 namespace DigraphCMS\UI\ButtonMenus;
 
+use DigraphCMS\HTML\DIV;
 use DigraphCMS\URL\URL;
 
-class ButtonMenu
+class ButtonMenu extends DIV
 {
-    protected $label, $myID;
-    /** @var ButtonMenuButton[] */
-    protected $buttons = [];
-    protected $target = '_frame';
+    /** @var DIV */
+    protected $buttons;
 
-    public function __construct(string $label = null, array $buttons = [])
+    public function __construct(array $buttons = [])
     {
-        $this->label = $label;
+        $this->buttons = (new DIV)->addClass('button-menu__buttons');
+        $this->addChild($this->buttons);
+        $this->addClass('button-menu');
         foreach ($buttons as $button) {
             if ($button instanceof ButtonMenuButton) {
                 $this->addButton($button);
@@ -24,37 +25,21 @@ class ButtonMenu
         }
     }
 
-    /**
-     * Set the data-target attribute
-     *
-     * @param string $target
-     * @return static
-     */
     public function setTarget(string $target)
     {
-        $this->target = $target;
+        $this->setData('target', $target);
         return $this;
     }
 
     public function addButton(ButtonMenuButton $button)
     {
-        $this->buttons[] = $button;
+        $this->buttons->addChild($button);
     }
 
     public function newButton(string $label, callable|URL $callback_or_url, array $classes = []): ButtonMenuButton
     {
-        return $this->buttons[] = new ButtonMenuButton($label, $callback_or_url, $classes);
-    }
-
-    public function __toString()
-    {
-        $out = "<nav class='button-menu'>";
-        $out .= "<div class='button-menu__buttons'>";
-        foreach ($this->buttons as $button) {
-            $out .= $button;
-        }
-        $out .= "</div>";
-        $out .= "</nav>";
-        return $out;
+        $button = new ButtonMenuButton($label, $callback_or_url, $classes);
+        $this->addButton($button);
+        return $button;
     }
 }
