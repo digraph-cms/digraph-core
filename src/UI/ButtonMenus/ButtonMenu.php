53e57063c43893bd7613a175f14df71be3940a97
Joby Elliott <jelliott@cs.unm.edu>
2024-03-29 10:06:46 -0600

    button menu refactor

--- src/UI/ButtonMenus/ButtonMenu.php
+++ src/UI/ButtonMenus/ButtonMenu.php
@@ -2,17 +2,11 @@
 
 namespace DigraphCMS\UI\ButtonMenus;
 
-use DigraphCMS\Context;
-use DigraphCMS\Exception;
-use DigraphCMS\ExceptionLog;
-use DigraphCMS\Session\Cookies;
 use DigraphCMS\URL\URL;
 
 class ButtonMenu
 {
-    protected static $id = 0;
     protected $label, $myID;
-    protected $csrf = false;
     /** @var ButtonMenuButton[] */
     protected $buttons = [];
     protected $target = '_frame';
@@ -20,7 +14,6 @@ class ButtonMenu
     public function __construct(string $label = null, array $buttons = [])
     {
         $this->label = $label;
-        $this->myID = static::$id++;
         foreach ($buttons as $button) {
             if ($button instanceof ButtonMenuButton) {
                 $this->addButton($button);
@@ -53,70 +46,15 @@ class ButtonMenu
         return $this->buttons[] = new ButtonMenuButton($label, $callback_or_url, $classes);
     }
 
-    public function id(): string
-    {
-        return 'buttonmenu_' . md5($this->myID);
-    }
-
-    public function csrf(bool $csrf = null): bool
-    {
-        if ($csrf !== null) {
-            $this->csrf = $csrf;
-        }
-        return $this->csrf;
-    }
-
-    public function token()
-    {
-        if ($this->csrf) {
-            return Cookies::csrfToken();
-        } else {
-            return md5(Context::url() . $_SERVER['HTTP_USER_AGENT']);
-        }
-    }
-
-    public function tokenID()
-    {
-        return md5($this->id()) . "_token";
-    }
-
-    public function submitID()
-    {
-        return md5($this->id()) . "_submit";
-    }
-
-    public function execute()
-    {
-        if (@$_POST[$this->submitID()] != '1') {
-            return;
-        } elseif (@$_POST[$this->tokenID()] == $this->token()) {
-            foreach ($this->buttons as $button) {
-                if (@$_POST[$button->id()]) {
-                    $button->execute();
-                }
-            }
-            Context::response()->redirect(Context::url());
-        } else {
-            ExceptionLog::log(new Exception('Button menu token mismatch', [
-                '$_POST[$this->tokenID()]' => $_POST[$this->id()],
-                '$this->token()' => $this->token(),
-            ]));
-        }
-    }
-
     public function __toString()
     {
-        ob_start();
-        $this->execute();
-        echo "<nav class='button-menu'><form method='POST' action='" . Context::url() . "' data-target='" . $this->target . "' id='" . $this->id() . "'>";
-        echo "<div class='button-menu__buttons'>";
+        $out = "<nav class='button-menu'>";
+        $out .= "<div class='button-menu__buttons'>";
         foreach ($this->buttons as $button) {
-            echo $button;
+            $out .= $button;
         }
-        echo "</div>";
-        echo "<input type='hidden' name='" . $this->tokenID() . "' value='" . $this->token() . "'>";
-        echo "<input type='hidden' name='" . $this->submitID() . "' value='1'>";
-        echo "</form></nav>";
-        return ob_get_clean();
+        $out .= "</div>";
+        $out .= "</nav>";
+        return $out;
     }
 }
--- src/UI/ButtonMenus/ButtonMenuButton.php
+++ src/UI/ButtonMenus/ButtonMenuButton.php
@@ -2,41 +2,26 @@
 
 namespace DigraphCMS\UI\ButtonMenus;
 
-use DigraphCMS\HTTP\RedirectException;
+use DigraphCMS\UI\CallbackLink;
 use DigraphCMS\URL\URL;
 
-class ButtonMenuButton
+class ButtonMenuButton extends CallbackLink
 {
-    protected static $id = 1;
-    protected $myID, $label, $callback_or_url, $classes;
-
     public function __construct(string $label, callable|URL $callback_or_url, array $classes = [])
     {
-        $this->label = $label;
-        $this->callback_or_url = $callback_or_url;
-        $this->classes = $classes;
-        $this->myID = static::$id++;
-    }
-
-    public function id(): string
-    {
-        return 'button_' . $this->myID;
-    }
-
-    public function execute()
-    {
-        if ($this->callback_or_url instanceof URL) {
-            throw new RedirectException($this->callback_or_url);
+        if ($callback_or_url instanceof URL) {
+            $url = $callback_or_url;
+            $callback = function () {
+            };
         } else {
-            call_user_func($this->callback_or_url);
+            $callback = $callback_or_url;
+            $url = null;
         }
-    }
-
-    public function __toString()
-    {
-        $classes = $this->classes;
-        $classes[] = 'button-menu-button';
-        $classes = implode(' ', $classes);
-        return "<input type='submit' name='" . $this->id() . "' value='" . $this->label . "' class='$classes'>";
+        parent::__construct($callback, $url);
+        $this->addClass('button');
+        $this->addChild($label);
+        foreach ($classes as $class) {
+            $this->addClass($class);
+        }
     }
 }
