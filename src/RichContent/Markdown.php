09ba6be757905e0950aca60edba75e8b411673c3
Joby Elliott <elhober@unm.edu>
2024-07-01 13:19:32 -0600

    switching markdown library

--- src/RichContent/Markdown.php
+++ src/RichContent/Markdown.php
@@ -2,20 +2,25 @@
 
 namespace DigraphCMS\RichContent;
 
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use League\CommonMark\MarkdownConverter;
+use N0sz\CommonMark\Marker\MarkerExtension;
+
 class Markdown
 {
     public static function parse(string $input): string
     {
-        // TODO: Look into replacing/updating parsedown to avoid deprecations
-        return @static::parsedown()->text($input);
-    }
-
-    protected static function parsedown(): ParsedownDigraph
-    {
-        static $parsedown;
-        if (!$parsedown) {
-            $parsedown = new ParsedownDigraph();
+        static $converter = null;
+        if (!$converter) {
+            $config = [];
+            $environment = new Environment($config);
+            $environment->addExtension(new CommonMarkCoreExtension());
+            $environment->addExtension(new GithubFlavoredMarkdownExtension());
+            $environment->addExtension(new MarkerExtension());
+            $converter = new MarkdownConverter($environment);
         }
-        return $parsedown;
+        return $converter->convert($input);
     }
 }
