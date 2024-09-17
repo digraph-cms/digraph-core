d1dd084cabbc4f64702aabbd25f8f86a1d4f46d5
Joby Elliott <jelliott@cs.unm.edu>
2024-09-17 13:34:45 -0600

    fix for context in page indexing

--- src/Content/Page.php
+++ src/Content/Page.php
@@ -2,6 +2,7 @@
 
 namespace DigraphCMS\Content;
 
+use DigraphCMS\Context;
 use DigraphCMS\Search\Search;
 
 class Page extends AbstractPage
@@ -10,12 +11,15 @@ class Page extends AbstractPage
     public function cronJob_index_pages()
     {
         $body = $this->richContent('body');
-        if ($body) Search::indexURL($this->uuid(), $this->url(), $this->name(), $body->html());
+        if ($body) {
+            Context::beginPageContext($this);
+            Search::indexURL($this->uuid(), $this->url(), $this->name(), $body->html());
+            Context::end();
+        }
     }
 
     public function routeClasses(): array
     {
         return array_unique([$this->class(), 'page', '_any']);
     }
-
 }
