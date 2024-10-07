ef7e805539870b0204a1ddaf500ae6689c9f5b5f
Joby Elliott <jelliott@cs.unm.edu>
2024-10-07 12:12:45 -0600

    bug fix

--- routes/@_any/@wildcard_filestore.action.php
+++ routes/@_any/@wildcard_filestore.action.php
@@ -2,29 +2,26 @@
 
 use DigraphCMS\Content\Filestore;
 use DigraphCMS\Content\FilestoreFile;
+use DigraphCMS\Content\Pages;
 use DigraphCMS\Context;
 use DigraphCMS\HTTP\HttpError;
 use DigraphCMS\RichMedia\RichMedia;
 use DigraphCMS\UI\Breadcrumb;
 
 // locate file
-$page = Context::page();
-$file = Filestore::select()
-    ->like('parent', $page->uuid(), false, true)
-    ->where('uuid', Context::url()->actionSuffix())
-    ->fetch();
-if (!$file) {
-    foreach (RichMedia::select(Context::pageUUID()) as $media) {
-        $file = Filestore::select()
-            ->like('parent', $media->uuid(), false, true)
-            ->where('uuid', Context::url()->actionSuffix())
-            ->fetch();
-    }
-}
-
-// check that it was found
+$file = Filestore::get(Context::url()->actionSuffix());
 if (!$file) throw new HttpError(404);
 assert($file instanceof FilestoreFile);
+
+// check that its parent belongs at the current location
+$page = null;
+$media = null;
+if ($page = Pages::get($file->parentUUID())) {
+    $media = null;
+} elseif ($media = RichMedia::get($file->parentUUID())) {
+    $page = Pages::get($media->parentUUID());
+}
+if (!$page || $page->uuid() != Context::pageUUID()) throw new HttpError(404);
 
 // render file
 Breadcrumb::setTopName($file->filename());
