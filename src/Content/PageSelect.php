24859979218c578b3ff2b99dd8a941acc18f25dc
Joby Elliott <jelliott@cs.unm.edu>
2025-01-22 11:59:24 -0700

    static analysis improvement

--- src/Content/PageSelect.php
+++ src/Content/PageSelect.php
@@ -4,6 +4,10 @@ namespace DigraphCMS\Content;
 
 use DigraphCMS\DB\AbstractMappedSelect;
 
+/**
+ * @template T of AbstractPage
+ * @extends AbstractMappedSelect<T>
+ */
 class PageSelect extends AbstractMappedSelect
 {
     function doRowToObject(array $row): ?object
