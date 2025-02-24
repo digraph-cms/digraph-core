15ddcad5ed6a49077cae75cefa6d00d5044c79ca
Joby Elliott <jelliott@cs.unm.edu>
2025-02-24 11:28:45 -0700

    deobfuscation character encoding fix

--- media/scripts/obfuscation.js
+++ media/scripts/obfuscation.js
@@ -8,7 +8,13 @@
     function unobfuscate(target) {
         Array.from(target.getElementsByClassName('obfuscated--base64'))
             .forEach(element => {
-                element.innerHTML = atob(element.firstChild.innerHTML);
+                const base64 = atob(element.firstChild.innerHTML);
+                const bytes = new Uint8Array(base64.length);
+                for (let i = 0; i < base64.length; i++) {
+                    bytes[i] = base64.charCodeAt(i);
+                }
+                const decoder = new TextDecoder('utf-8');
+                element.innerHTML = decoder.decode(bytes);
                 element.classList.remove('obfuscated--base64');
                 element.classList.add('obfuscated--decoded');
                 element.dispatchEvent(
