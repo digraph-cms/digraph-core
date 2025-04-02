eab1ce1a4c69df224628198f3d8b4e1997bd3958
Joby Elliott <jelliott@cs.unm.edu>
2025-04-02 08:58:56 -0600

    enhance 401 error page to specify sign-in sources and improve messaging

--- routes/~error/401.action.php
+++ routes/~error/401.action.php
@@ -2,16 +2,28 @@
 
 use DigraphCMS\Content\Router;
 use DigraphCMS\Context;
+use DigraphCMS\Users\AbstractUserSource;
 use DigraphCMS\Users\Users;
 
 $user = Users::current();
 if (!$user) {
-    echo "<h1>Sign in to continue</h1>";
+    $sources = Users::sources();
+    if (count($sources) <= 1) {
+        $source = reset($sources);
+        echo "<h1>" . $source->name() . " sign in required</h1>";
+    } else {
+        $source_names = array_map(fn(AbstractUserSource $s) => '<strong>' . $s->name() . '</strong>', $sources);
+        $last = array_pop($source_names);
+        $source_names[] = array_pop($source_names) . ' or ' . $last;
+        $source_names = implode(', ', $source_names);
+        echo "<h1>Sign in required</h1>";
+        printf('<p>To view this page you will need to sign in with a %s account.</p>', $source_names);
+    }
     $signinUrl = Users::signinUrl(Context::request()->originalUrl());
-    echo "<p>You must <a href='$signinUrl' class='button'>sign in</a> to view this page</p>";
-}else {
+    echo "<p><a href='$signinUrl' class='button'>Sign in</a> to view this page</p>";
+} else {
     echo "<h1>Access denied</h1>";
-    echo "<p>You have been denied access to this page.</p>";
+    echo "<p>Your account has been denied access to this page.</p>";
 }
 
 if ($message = Context::data('error_message')) {
