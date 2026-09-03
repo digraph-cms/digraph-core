0622b3e2873a1f9bedb51951220b4b5f72c6d622
Joby Elliott <jelliott@cs.unm.edu>
2026-09-03 12:37:15 -0600

    migration change

--- phinx/migrations/20260903175039_convert_to_utf_8_mb_4.php
+++ phinx/migrations/20260903175039_convert_to_utf_8_mb_4.php
@@ -12,8 +12,6 @@ final class ConvertToUtf8Mb4 extends AbstractMigration
         if ($this->getAdapter()->getAdapterType() !== 'mysql')
             return;
         $this->execute('SET FOREIGN_KEY_CHECKS = 0');
-        $db = $this->getAdapter()->getOption('name');
-        $this->execute("ALTER DATABASE `{$db}` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci");
         $rows = $this->fetchAll(
             "SELECT TABLE_NAME FROM information_schema.TABLES
                     WHERE TABLE_SCHEMA = DATABASE()
@@ -24,6 +22,8 @@ final class ConvertToUtf8Mb4 extends AbstractMigration
                 $t = $row[0];
                 $this->execute("ALTER TABLE `{$t}` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci");
             }
+            $db = $this->getAdapter()->getOption('name');
+            $this->execute("ALTER DATABASE `{$db}` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci");
         }
         finally {
         }
